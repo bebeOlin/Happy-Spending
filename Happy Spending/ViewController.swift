d80469e4c75e1520de8489305bbcc4a35ff75dc3
Bruce Bolin <Bruce@gingerail.games>
2021-10-01 20:19:41 -0700

    Initial Commit

---
+++ Happy Spending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Happy Spending
+//
+//  Created by Bruce Bolin on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
